ab76930097c94a26ca56b0521f9ca82a4d773b96
Renato Mangini <mangini@gmail.com>
2012-11-29 19:14:39 -0200

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-novidadeshtml5
-==============
 
-Apresentação feita no DevFest Brasil 2012
+DevFest Brazil 2012
+===================
+
+
+Slide deck presented at DevFest Brazil 2012, with some new features recently landed or soon landing in HTML5 planet. Some topics (pt\_BR only):
+
+* CSS adequado a Web Apps (CSS Regions, Exclusions e Flexbox)
+* CSS dinâmico (CSS Filters, CSS Shaders)
+* JavaScript multithread (Web Workers)
+* Acessando um Filesystem (FileSystem API)
+* Downloads sem servidor
+* Acesso ao hardware (WebRTC, Speech API)
+* <audio> "sexy" (WebAudio API, gravando audio com WebRTC)
+* Stream Multimidia (WebSockets binários)
+
---
+++ slide_config.js
@@ -0,0 +1,31 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Construindo aplicativos HTML5',
+    subtitle: 'Recursos que (talvez) você não conheça',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: false, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/chrome-logo-tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata',
+      'Cabin Sketch:400',
+      'Muli'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Renato Mangini',
+    gplus: 'http://profiles.google.com/mangini',
+    title: 'Chrome Team',
+    company: 'Google',
+    twitter: 'renatomangini',
+    www: 'http://renatomangini.com',
+    github: 'https://github.com/mangini'
+  }]
+};
+
